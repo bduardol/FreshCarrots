99f8677e392f605f23bea052e68247b026f82259
Bruno Eduardo <102605344+bduardol@users.noreply.github.com>
2023-11-06 19:30:42 -0300

    Update models.py
    Adicionei uns models, mas ainda esta incopleto.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class user(models.Model):
+  firt_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  email = models.EmailField(max_length=100)
+  cpf = models.CharField(unique=True, max_length=11,  primary_key=True)
+
+class book(models.Model):
+  name = models.CharField(max_length=30)
+  author = models.CharField(max_length=30)
+  
+  
